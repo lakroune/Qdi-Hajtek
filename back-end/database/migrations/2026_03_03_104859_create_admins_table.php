5fc0cc0d0b7b924c617164cffa3e2c69fd98765b
lakroune <lakroune00@gmail.com>
2026-03-18 10:26:26 +0000

    refactor: remove unused migration files for clients, personal access tokens, and admins to streamline database structure

--- back-end/database/migrations/2026_03_03_104859_create_admins_table.php
+++ back-end/database/migrations/2026_03_03_104859_create_admins_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('admins', function (Blueprint $table) {
-            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->primary('user_id');
             $table->timestamps();
         });
     }
