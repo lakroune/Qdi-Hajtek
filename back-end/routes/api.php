63112bee7ef6a8443d859c0b5862b381f66f2d21
lakroune <lakroune00@gmail.com>
2026-03-18 10:40:42 +0000

    refactor: update JWT-auth package version and simplify welcome page layout for improved clarity and user experience

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,27 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/verifierEmail', [AuthController::class, 'verifierEmail']);
-Route::post('/gererRenvoi', [AuthController::class, 'gererRenvoi']);
-Route::middleware('auth:sanctum')->get('/utilisateur', [AuthController::class, 'donneesUtilisateur']);
-Route::post('/utilisateur', [ClientController::class, 'getInfo']);
-
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-
-    Route::middleware('role:artisan')->group(function () {
-        // Route::post('/services', [ServiceController::class, 'store']);
-        // Route::post('/portfolio', [PortfolioController::class, 'add']);
-    });
-
-    Route::middleware('role:client')->group(function () {
-        // Route::post('/offres', [OffreController::class, 'store']);
-    });
-});
