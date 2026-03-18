4b542c9d4ebb8e436b49580621eb3e388353fa1b
lakroune <lakroune00@gmail.com>
2026-03-18 12:50:54 +0000

    refactor: rename gererRenvoi to generateCode, enhance email verification code generation, and improve response messages for clarity

--- back-end/app/Http/Controllers/Api/AuthController.php
+++ back-end/app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\auth\LoginRequest;
 use App\Http\Requests\auth\RegisterRequest;
 use App\Http\Requests\auth\VerifierEmailRequest;
+use App\Http\Requests\GenerateCodeRequest;
 use App\Mail\VerificationCodeMail;
 use App\Models\Role;
 use App\Models\User;
@@ -117,28 +118,39 @@ class AuthController extends Controller
         ], 200);
     }
 
-    public function gererRenvoi(Request $request)
+    public function generateCode(GenerateCodeRequest $request)
     {
-        $request->validate([
-            'email' => 'required|email|exists:users,email'
-        ]);
+        $data = $request->validated();
 
-        $user = User::where('email', $request->email)->first();
-
-        if ($user->email_verified_at) {
-            return response()->json(['message' => 'Cet email est déjà vérifié'], 400);
+        $user = User::where('email', $data['email'])->first();
+        if (!$user) {
+            return response()->json(
+                ['message' => 'invalid email'],
+                404
+            );
         }
 
-        $nouveauCode = str_pad(random_int(0, 9999), 4, '0', STR_PAD_LEFT);
+        if ($user->email_verified_at) {
+            return response()->json(
+                ['message' => 'email already verified'],
+                400
+            );
+        }
 
-        $user->code_verification = $nouveauCode;
+        $new_code = random_int(100000, 999999);
+        $user->code_verification = $new_code;
         $user->save();
 
+
         try {
-            Mail::to($user->email)->send(new VerificationCodeMail($nouveauCode, $user));
-            return response()->json(['message' => 'Nouveau code envoyé avec succès']);
+            Mail::to($user->email)->send(new VerificationCodeMail($user->code_verification, $user));
+            return response()->json([
+                'message' => 'email sent successfully'
+            ], 200);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'Erreur lors de l\'envoi de l\'email'], 500);
+            return response()->json([
+                'message' => 'something went wrong'
+            ], 500);
         }
     }
     public function logout(Request $request)
---
+++ back-end/app/Http/Requests/GenerateCodeRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class GenerateCodeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|exists:users,email'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'email is required',
+            'email.exists' => 'email is not found',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'success' => false,
+                'message' => 'Validation errors',
+                'data' => $validator->errors()
+            ], 422)
+        );
+    }
+}
