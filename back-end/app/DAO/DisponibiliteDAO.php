e48d0799e88c4ef559ed38e6ffbfcc8f1d5c3493
lakroune <lakroune00@gmail.com>
2026-03-20 16:21:24 +0000

    feat: enhance DisponibiliteDAO with methods for bulk creation, updating, and deletion of disponibilites, and improve retrieval by artisan ID

--- back-end/app/DAO/DisponibiliteDAO.php
+++ back-end/app/DAO/DisponibiliteDAO.php
@@ -2,13 +2,56 @@
 
 namespace App\DAO;
 
+use App\Models\Disponibilite;
+use Illuminate\Support\Facades\DB;
+
 class DisponibiliteDAO
 {
     /**
-     * Create a new class instance.
+     * createMany  
      */
-    public function __construct()
+    public function createMany(int $artisanId, array $data)
     {
-        //
+        return DB::transaction(function () use ($artisanId, $data) {
+            $disponibilites = [];
+
+            foreach ($data as $item) {
+                $disponibilites[] = [
+                    'artisan_id'   => $artisanId,
+                    'jour_semaine' => $item['jour'],
+                    'heure_debut'  => $item['debut'],
+                    'heure_fin'    => $item['fin']
+                ];
+            }
+
+            return Disponibilite::insert($disponibilites);
+        });
+    }
+
+    /**
+     * update
+     */
+    public function update(int $id, array $data)
+    {
+        $disponibilite = Disponibilite::findOrFail($id);
+        return $disponibilite->update($data);
+    }
+
+    /**
+     *  delete all disponibilites
+     */
+    public function deleteAllByArtisan(int $artisanId)
+    {
+        return Disponibilite::where('artisan_id', $artisanId)->delete();
+    }
+
+    /**
+     * get all disponibilites
+     */
+    public function getAllByArtisan(int $artisanId)
+    {
+        return Disponibilite::where('artisan_id', $artisanId)
+            ->orderByRaw("FIELD(jour_semaine, 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche')")
+            ->get();
     }
 }
