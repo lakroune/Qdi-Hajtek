025ce513e210db825e39f108f850beb1d00b4115
lakroune <lakroune00@gmail.com>
2026-03-21 12:51:15 +0000

    feat: trigger PropositionCreated event after creating a new proposition to enhance event-driven architecture

--- back-end/app/Services/PropositionService.php
+++ back-end/app/Services/PropositionService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\DAO\PropositionDAO;
 use App\DTO\PropositionDTO;
+use App\Events\PropositionCreated;
 
 class PropositionService
 {
@@ -17,6 +18,9 @@ class PropositionService
 
     public function createProposition(PropositionDTO $propositionDTO)
     {
-        return $this->propositionDAO->create($propositionDTO->toArray());
+        $proposition = $this->propositionDAO->create($propositionDTO->toArray());
+
+        event(new PropositionCreated($proposition));
+        return $proposition;
     }
 }
