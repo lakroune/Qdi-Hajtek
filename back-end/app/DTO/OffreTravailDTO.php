19d868addb1531dd2e1e3b5bd2db525169356679
lakroune <lakroune00@gmail.com>
2026-03-21 11:07:18 +0000

    feat: enhance OffreTravailDTO by adding is_completed property and updating default statut; improve fromRequest method to include validated request data

--- back-end/app/DTO/OffreTravailDTO.php
+++ back-end/app/DTO/OffreTravailDTO.php
@@ -18,19 +18,40 @@ class OffreTravailDTO
         public readonly string $dateLimite,
         public readonly string $typeRemuneration,
         public readonly string $niveauUrgence,
-        public readonly string $statut = 'ouvert'
+        public readonly string $statut = 'en_cours',
+        public readonly bool $is_completed = false
 
     ) {
         //
     }
 
-    public static function  fromRequest(Request $request)
+    public static function  fromRequest($request)
     {
-        // 
+        return new self(
+            clientId: $request->user()->client->id,
+            categorieId: $request->valivalidated('categorie_id'),
+            titre: $request->valivalidated('titre'),
+            description: $request->valivalidated('description'),
+            budgetEstime: $request->valivalidated('budget_estime'),
+            dateLimite: $request->valivalidated('date_limite'),
+            typeRemuneration: $request->valivalidated('type_remuneration'),
+            niveauUrgence: $request->valivalidated('niveau_urgence'),
+        );
     }
 
     public function toArray()
     {
-        // 
+        return [
+            'client_id' => $this->clientId,
+            'categorie_id' => $this->categorieId,
+            'titre' => $this->titre,
+            'description' => $this->description,
+            'budget_estime' => $this->budgetEstime,
+            'date_limite' => $this->dateLimite,
+            'type_remuneration' => $this->typeRemuneration,
+            'niveau_urgence' => $this->niveauUrgence,
+            'statut' => $this->statut,
+            'is_completed' => $this->is_completed
+        ];
     }
 }
---
+++ back-end/app/Http/Requests/StroreOffreTravailRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StroreOffreTravailRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+
+        return [
+            'service_id' => 'required|exists:services,id',
+            'categorie_id' => 'required|exists:categories,id',
+            'titre' => 'required|string',
+            'description' => 'required|string',
+            'budget_estime' => 'required|numeric',
+            'date_limite' => 'required|date',
+            'type_remuneration' => 'required|in:prix_fixe,prix_heure',
+            'niveau_urgence' => 'required|in:faible,moyen,urgent',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'service_id.required' => 'Le service est requis.',
+            'categorie_id.required' => 'La catégorie est requise.',
+            'titre.required' => 'Le titre est requis.',
+            'description.required' => 'La description est requise.',
+            'budget_estime.required' => 'Le budget estimé est requis.',
+            'date_limite.required' => 'La date limite est requise.',
+            'type_remuneration.required' => 'Le type de remuneration est requis.',
+            'niveau_urgence.required' => 'Le niveau d\'urgence est requis.',
+            'service_id.exists' => 'Le service n\'existe pas.',
+            'categorie_id.exists' => 'La catégorie n\'existe pas.',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'success' => false,
+                'message' => 'Validation errors',
+                'data' => $validator->errors()
+            ], 422)
+        );
+    }
+}
