4c89cea373d1c52d9524e6d73e992fd8d4d5ff5a
lakroune <lakroune00@gmail.com>
2026-04-01 15:26:38 +0100

    refactor: enhance OffreTravailDAO find method to include filtered propositions and ensure only open offers are retrieved; update findByClient method for clarity

--- back-end/app/DAO/OffreTravailDAO.php
+++ back-end/app/DAO/OffreTravailDAO.php
@@ -41,7 +41,15 @@ class OffreTravailDAO
 
     public function find($id)
     {
-        return OffreTravail::with('images', 'categorie', 'client.user', 'propositions.artisan.user.client')->findOrFail($id);
+        return OffreTravail::with([
+            'images',
+            'categorie',
+            'client.user',
+            'propositions' => function ($prop) {
+                $prop->where('statut_proposition', 'en_attente')->with('artisan.user.client');
+            }
+        ])->where('statut', 'ouvert')
+            ->findOrFail($id);
     }
 
     public function getAll()
@@ -50,7 +58,7 @@ class OffreTravailDAO
     }
     public function findByClient($id)
     {
-        return OffreTravail::where('client_id', $id)->with('categorie' )->withCount('propositions')->get();
+        return OffreTravail::where('client_id', $id)->with('categorie')->withCount('propositions')->get();
     }
     public function findByCategorie($id)
     {
