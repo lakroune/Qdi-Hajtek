dd05bd83f53504bdd101656b272dbb3115998c1b
lakroune <lakroune00@gmail.com>
2026-04-06 00:52:24 +0100

    feat: refactor ArtisanResource to enhance data structure; update response fields for improved clarity and include additional client and artisan details

--- back-end/app/Http/Resources/ArtisanResource.php
+++ back-end/app/Http/Resources/ArtisanResource.php
@@ -14,24 +14,29 @@ class ArtisanResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-    
+
         return [
             'id' => $this->id,
-            'specialite' => $this->specialite,
-            'bio' => $this->bio,
-            'experience' => $this->experience,
-            'is_verified' => (bool) $this->is_verified,
-            'note' => $this->note,
-            'rayon_action' => $this->rayon_action,
-            
-            // Détails de l'utilisateur
-            'user' => [
-                'id' => $this->user->id,
-                'firstname' => $this->user->firstname,
-                'lastname' => $this->user->lastname,
-                'email' => $this->user->email,
-                'city' => $this->user->city,
-            ],
+            'lastname' => $this->lastname,
+            'firstname' => $this->firstname,
+            'email' => $this->email,
+            'city' => $this->city,
+            'email_verified_at' => $this->email_verified_at,
+            'created_at' => $this->created_at,
+            'statut' => $this->client->statut ?? null,
+            'phone' => $this->client->phone ?? null,
+            'cin' => $this->client->cin ?? null,
+            'avatar' => $this->client->avatar ?? null,
+            'address' => $this->client->address ?? null,
+            'rib' => $this->client->rib ?? null,
+            'specialite' => $this->artisan->specialite ?? null,
+            'bio' => $this->artisan->bio ?? null,
+            'experience' => $this->artisan->experience ?? null,
+            'is_verified' => $this->artisan ? (bool)$this->artisan->is_verified : false,
+            'note' => $this->artisan->note ?? "0.0",
+            'rayon_action' => $this->artisan->rayon_action ?? 0,
+            // 'disponibilites' => DisponibiliteResource::collection($this->disponibilites),
+            // 'services' => ServiceResource::collection($this->services)
         ];
     }
 }
